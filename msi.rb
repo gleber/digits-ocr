c033318ce4c4f6c481a58c613b251f539efae632
Gleb Peregud <gleber.p@gmail.com>
2011-04-04 14:40:15 +0200

    make it work with ruby 1.9.2

--- msi.rb
+++ msi.rb
@@ -5,9 +5,9 @@ class MSI < Gtk::Window
 
   FILE_IMAGE_TRAIN = 'train-images.idx3-ubyte'
   FILE_LABEL_TRAIN = 'train-labels.idx1-ubyte'
-  FILE_IMAGE_TEST = 't10k-images.idx3-ubyte'
-  FILE_LABEL_TEST = 't10k-labels.idx1-ubyte'
-  
+  FILE_IMAGE_TEST = 't10k-images-idx3-ubyte'
+  FILE_LABEL_TEST = 't10k-labels-idx1-ubyte'
+
   def initialize
     super
 
@@ -20,8 +20,8 @@ class MSI < Gtk::Window
     @pixbuf = Gdk::Pixbuf.new(Gdk::Pixbuf::COLORSPACE_RGB, false, 8, 28, 28)
     @image = Gtk::Image.new @pixbuf.scale(280,280,Gdk::Pixbuf::INTERP_NEAREST)
     add @image
-    setImage 
-    
+    setImage
+
     signal_connect "destroy" do
       Gtk.main_quit
     end
@@ -33,20 +33,21 @@ class MSI < Gtk::Window
       when 'Left' then @current -= 1; @current = @image_test.size-1 if @current < 0; setImage;
       end
     end
-          
+
     set_window_position Gtk::Window::POS_CENTER
     show_all
   end
-  
+
   def setImage
     str = @pixbuf.pixels
     str.length.times do |i| str[i] = @image_test[@current][i/3] end
-    @pixbuf.set_pixels(str)
+    @pixbuf.pixels = str
     @image.pixbuf = @pixbuf.scale(280,280,Gdk::Pixbuf::INTERP_NEAREST)
     set_title("MSI, img ##{@current} => #{@label_test[@current]}")
   end
-  
+
   def loadIDX(filename)
+    x = 0
     File.open(filename, "rb") do |file|
       buf = file.read(3)
       # check for magic number (and data type)
@@ -55,16 +56,16 @@ class MSI < Gtk::Window
       dim = file.getbyte
       # check for dimension count
       raise 'ERROR: Unexpected number of dimensions' unless dim > 0
-      ary = Array.new(msb2int(file.read(4)))
+      count = file.read(4).unpack('N')[0]
+      ary = Array.new(count)
       if dim == 1
         # load label data
         ary.size.times do |i|
           ary[i] = file.getbyte
         end
       elsif dim == 3
-        w = msb2int(file.read(4))
-        h = msb2int(file.read(4))
-        # load image data
+        w = file.read(4).unpack('N')[0]
+        h = file.read(4).unpack('N')[0]
         ary.size.times do |i|
           ary[i] = file.read(w*h)
         end
@@ -72,19 +73,11 @@ class MSI < Gtk::Window
         raise 'ERROR: Unexpected number of dimensions'
       end
       # check for expected end of file
-      raise 'ERROR: Excessive data in file' unless file.getbyte.nil?
+      raise 'ERROR: Excessive data in file' unless file.getbyte().nil?
       return ary
     end
   end
-  
-  def msb2int(arg)
-    ret = 0
-    arg.length.times do |i|
-      ret = ret * 256 + arg[i]
-    end
-    return ret
-  end
-  
+
 end
 
 Gtk.init
